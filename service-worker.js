da5cd75ced46ea887870ae7a51334f383d0cbe50
rahmatdev09 <164139419+rahmatdev09@users.noreply.github.com>
2026-02-24 08:19:40 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -14,3 +14,17 @@ self.addEventListener("fetch", (event) => {
     }),
   );
 });
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
